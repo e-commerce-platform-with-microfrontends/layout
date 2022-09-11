b1b2ab26e667f8f669cf71ea54dcf99e0bdd4eb9
Vishal Sharma <vishal.sharma2@bp.com>
2022-09-11 18:12:54 +0530

    sending token in fetch cart call

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,7 @@ import * as S from './Header.styles';
 
 export default () => {
   const [itemsInCart, setItemsInCart] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') || false);
 
   const addToCartEventlistener = async ({ detail }) => {
     const newItemInCart = { item: { id: detail.itemId, price: detail.price } };
@@ -14,7 +15,8 @@ export default () => {
       const itemAdded = await fetch(`${API_ROOT.DEV}/cart/1/items`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'auth-token': localStorage.getItem('authToken'),
         },
         body: JSON.stringify(newItemInCart)
       }).then(res => res.json());
@@ -29,6 +31,9 @@ export default () => {
     try {
       await fetch(`${API_ROOT.DEV}/items/${detail.itemId}`, {
         method: 'DELETE',
+        headers: {
+          'auth-token': localStorage.getItem('authToken'),
+        }
       }).then(res => res.json());
 
       setItemsInCart((itemsInCart) => itemsInCart.filter(item => item.id !== detail.itemId));
@@ -38,7 +43,11 @@ export default () => {
   };
 
   useEffect(async () => {
-    const itemsInCart = await fetch(`${API_ROOT.DEV}/cart/1/items`).then(res => res.json());
+    const itemsInCart = await fetch(`${API_ROOT.DEV}/cart/1/items`, {
+      headers: {
+        'auth-token': localStorage.getItem('authToken'),
+      }
+    }).then(res => res.json());
 
     setItemsInCart(itemsInCart);
   }, []);
@@ -52,6 +61,14 @@ export default () => {
     }
   }, []);
 
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.setItem('isLoggedIn', false);
+    localStorage.removeItem('authToken');
+    setIsLoggedIn(false);
+    window.location.href = "http://localhost:3000/products/";
+  }
+
   return (
     <S.Header>
       <div className='container'>
@@ -59,13 +76,21 @@ export default () => {
           <a href="http://localhost:3000/products/">
             <Heading4>CBP Marketplace</Heading4>
           </a>
-          <S.CartLink id="go-to-cart" href="http://localhost:3000/cart/">
-            Cart
-            {
-              itemsInCart.length > 0 &&
-              <S.CartCount id="cart-count">{itemsInCart.length}</S.CartCount>
-            }
-          </S.CartLink>
+          {
+            isLoggedIn ?
+              <>
+                <S.CartLink id="go-to-cart" href="http://localhost:3000/cart/">
+                  Cart
+                  {
+                    itemsInCart.length > 0 &&
+                    <S.CartCount id="cart-count">{itemsInCart.length}</S.CartCount>
+                  }
+                </S.CartLink>
+                <span id="Logout" onClick={logout}>Logout</span>
+              </>
+              :
+              <a id="go-to-login" href="http://localhost:3000/login/">Login</a>
+          }
         </S.Content>
       </div>
     </S.Header>
